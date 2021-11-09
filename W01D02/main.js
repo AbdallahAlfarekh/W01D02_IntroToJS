a81cfc2591ec4afdfe37278b26af8b63a81bce80
AbdallahAlfarekh <93886245+AbdallahAlfarekh@users.noreply.github.com>
2021-11-09 13:39:26 +0300

    Add files via upload

---
+++ W01D02/main.js
@@ -0,0 +1,77 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+// Q1 answer
+//14
+//1
+//27
+//255
+//47
+
+//Q2 amswer
+// const $Name = 'john' or 
+// we can not use @ in the begining of var
+//const 'age' = 10 //the var can not be string 
+//it should be const age = 10 or '10
+ //const const  = 'constant' //لا يمكن استخدام الكلمات المحجوزة
+ //const variable  1  // we can not use space in var
+ 
+ 
+ //Q3 Answer
+ let color = "green"
+ let positiveNumber = 4
+ let food = "Mansaf"
+ let PhoneNumber = "0096260685028"
+
+ //Q4 Answer
+ // 1- 1010
+ //2- 1010
+ //3- 205
+ //4- Hello1
+ //5- 1010
+ //6-  error
+ //7-   error
+ //8-  error
+ //9-  144
+
+ //Q5 Answer
+ let firstName = "Abdallah"
+ let LastName ="Alfarekh"
+ let gender = "male"
+ let nationality = "Jordanian"
+
+ //Q6 Answer
+
+ let secondsPerMonth = 30*24*60*60 //2592000
+
+ //Q7 Answer
+ let a = 29
+
+ //Q8 answer
+ let cofeeMony = tip + taxRate + coffeCupPrice 
+
+ let total = 2* cofeeMony // 12.36
+
+ //Q9 Answer
+ let AverageGrade = ((5*24) + (2 * 16) 
++ (23*29))/30 //27.3
+
+//Q10 Answer
+let length = 10
+let width = 5
+let recAria = length * width  //50
+
+//Q11 Answer
+let radius = 5
+let diameter = 2 * radius
+const Qeta = 3.14
+let circumference = diameter * Qeta //31.4
+
+//Q12 Answer 
+const Qaba = 1.8
+let CDegree = 30
+let Fehrn = Qaba * CDdegree //54
+
+
+
+
